42f0c7980a1c0bd33e20d8e224dcbc692d89dd56
Antonis Kalou <kalouantonis@gmail.com>
2016-05-25 16:20:50 +0300

    Added conditional compilation for python versions

--- heatshrink.c
+++ heatshrink.c
@@ -10,7 +10,32 @@ static PyMethodDef HeatshrinkMethods [] = {
     {NULL, NULL, 0, NULL} // Sentinel
 };
 
-PyMODINIT_FUNC initheatshrink()
+/************************************************************
+ * Initialization
+ ************************************************************/
+#if (PY_MAJOR_VERSION >= 3)
+static struct PyModuleDef HeatshrinkModule = {
+    PyModuleDef_HEAD_INIT,
+    "heatshrink",
+    NULL,
+    -1,
+    HeatshrinkMethods,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+
+PyMODINIT_FUNC
+PyInit_heatshrink(void)
+{
+		return PyModule_Create(&HeatshrinkModule);
+}
+
+#else /* Python 2 */
+PyMODINIT_FUNC
+initheatshrink(void)
 {
     (void) Py_InitModule("heatshrink", HeatshrinkMethods);
 }
+#endif
