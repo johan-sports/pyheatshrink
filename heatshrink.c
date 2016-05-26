2c2c795de033b1582de42a96cefecfacb9c834e1
Antonis Kalou <kalouantonis@gmail.com>
2016-05-26 10:04:41 +0300

    Fixed indentation

--- heatshrink.c
+++ heatshrink.c
@@ -32,12 +32,11 @@ PyHS_Encoder_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
 		if(self != NULL) {
 				heatshrink_encoder *hse = heatshrink_encoder_alloc(
 						DEFAULT_HEATSHRINK_WINDOW_SZ2,
-						DEFAULT_HEATSHRINK_LOOKAHEAD_SZ2
-				);
+						DEFAULT_HEATSHRINK_LOOKAHEAD_SZ2);
 
 				if(hse == NULL) {
 						Py_DECREF(self);
-						PyErr_SetString(PyExc_MemoryError, "failed to allocate encoder.");
+						PyErr_SetString(PyExc_MemoryError, "failed to allocate internal encoder.");
 						return NULL;
 				}
 
@@ -68,47 +67,47 @@ static PyTypeObject PyHS_EncoderType = {
 		"heatshrink.Encoder",       /* tp_name */
 		sizeof(PyHS_Encoder),       /* tp_basicsize */
 		0,                          /*tp_itemsize*/
-    (destructor)PyHS_Encoder_dealloc,   /*tp_dealloc*/
-    0,                          /*tp_print*/
-    0,                          /*tp_getattr*/
-    0,                          /*tp_setattr*/
-    0,                          /*tp_compare*/
-    0,                          /*tp_repr*/
-    0,                          /*tp_as_number*/
-    0,                          /*tp_as_sequence*/
-    0,                          /*tp_as_mapping*/
-    0,                          /*tp_hash */
-    0,                          /*tp_call*/
-    0,                          /*tp_str*/
-    0,                          /*tp_getattro*/
-    0,                          /*tp_setattro*/
-    0,                          /*tp_as_buffer*/
+		(destructor)PyHS_Encoder_dealloc,   /*tp_dealloc*/
+		0,                          /*tp_print*/
+		0,                          /*tp_getattr*/
+		0,                          /*tp_setattr*/
+		0,                          /*tp_compare*/
+		0,                          /*tp_repr*/
+		0,                          /*tp_as_number*/
+		0,                          /*tp_as_sequence*/
+		0,                          /*tp_as_mapping*/
+		0,                          /*tp_hash */
+		0,                          /*tp_call*/
+		0,                          /*tp_str*/
+		0,                          /*tp_getattro*/
+		0,                          /*tp_setattro*/
+		0,                          /*tp_as_buffer*/
 		Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
 		"Encoder objects",           /* tp_doc */
-    0,		                       /* tp_traverse */
-    0,		                       /* tp_clear */
-    0,		                       /* tp_richcompare */
-    0,		                       /* tp_weaklistoffset */
-    0,		                       /* tp_iter */
-    0,		                       /* tp_iternext */
-    PyHS_Encoder_methods,        /* tp_methods */
-    PyHS_Encoder_members,        /* tp_members */
-    0,                           /* tp_getset */
-    0,                           /* tp_base */
-    0,                           /* tp_dict */
-    0,                           /* tp_descr_get */
-    0,                           /* tp_descr_set */
-    0,                           /* tp_dictoffset */
-    (initproc)PyHS_Encoder_init, /* tp_init */
-    0,                           /* tp_alloc */
-    PyHS_Encoder_new,            /* tp_new */
+		0,		                       /* tp_traverse */
+		0,		                       /* tp_clear */
+		0,		                       /* tp_richcompare */
+		0,		                       /* tp_weaklistoffset */
+		0,		                       /* tp_iter */
+		0,		                       /* tp_iternext */
+		PyHS_Encoder_methods,        /* tp_methods */
+		PyHS_Encoder_members,        /* tp_members */
+		0,                           /* tp_getset */
+		0,                           /* tp_base */
+		0,                           /* tp_dict */
+		0,                           /* tp_descr_get */
+		0,                           /* tp_descr_set */
+		0,                           /* tp_dictoffset */
+		(initproc)PyHS_Encoder_init, /* tp_init */
+		0,                           /* tp_alloc */
+		PyHS_Encoder_new,            /* tp_new */
 };
 
 /************************************************************
  * Module definition
  ************************************************************/
 static PyMethodDef Heatshrink_methods [] = {
-    {NULL, NULL, 0, NULL} // Sentinel
+		{NULL, NULL, 0, NULL} // Sentinel
 };
 
 /************************************************************
