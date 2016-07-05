a40063e644272e69f3c9998a5fca1192aa519362
Antonis Kalou <kalouantonis@gmail.com>
2016-07-05 15:56:49 +0300

    Removed benchmark duplication

--- benchmark.py
+++ benchmark.py
@@ -2,48 +2,25 @@ import time
 import heatshrink
 
 
-def encode_bench(filename):
+def run_benchmark(filename, fn):
     with open(filename, 'rb') as fp:
         contents = fp.read()
 
     print('*** Reading 10,000 bytes ***')
     t0 = time.time()
-    heatshrink.encode(contents[:10000])
+    fn(contents[:10000])
     elapsed = time.time() - t0
     print('==> {}s elapsed'.format(elapsed))
 
     print('*** Reading 1,000,000 bytes ***')
     t0 = time.time()
-    heatshrink.encode(contents[:1000000])
+    fn(contents[:1000000])
     elapsed = time.time() - t0
     print('==> {}s elapsed'.format(elapsed))
 
     print('*** Reading whole file ({} bytes) ***'.format(len(contents)))
     t0 = time.time()
-    heatshrink.encode(contents)
-    elapsed = time.time() - t0
-    print('==> {}s elapsed'.format(elapsed))
-
-
-def decode_bench(filename):
-    with open(filename, 'rb') as fp:
-        contents = fp.read()
-
-    print('*** Reading 10,000 bytes ***')
-    t0 = time.time()
-    heatshrink.decode(contents[:10000])
-    elapsed = time.time() - t0
-    print('==> {}s elapsed'.format(elapsed))
-
-    print('*** Reading 1,000,000 bytes ***')
-    t0 = time.time()
-    heatshrink.decode(contents[:1000000])
-    elapsed = time.time() - t0
-    print('==> {}s elapsed'.format(elapsed))
-
-    print('*** Reading whole file ({} bytes) ***'.format(len(contents)))
-    t0 = time.time()
-    heatshrink.decode(contents)
+    fn(contents)
     elapsed = time.time() - t0
     print('==> {}s elapsed'.format(elapsed))
 
@@ -57,9 +34,9 @@ def print_block(msg, size=50):
 
 def main():
     print_block('Encode benchmarks')
-    encode_bench('plain_file.txt')
+    run_benchmark('plain_file.txt', heatshrink.encode)
     print_block('Decode benchmarks')
-    decode_bench('compressed_file.txt')
+    run_benchmark('compressed_file.txt', heatshrink.decode)
 
 
 if __name__ == '__main__':
