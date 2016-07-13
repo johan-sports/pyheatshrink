f6e1f1a1c80352634214f55dd247f372268f0bbd
Antonis Kalou <kalouantonis@gmail.com>
2016-07-13 18:40:26 +0300

    Removed heatshrink debug macro definitions

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from codecs import open
 
 heatshrink_module = Extension('heatshrink',
                               include_dirs=['.'],
-                              define_macros=[('HEATSHRINK_DEBUG_LOGS', 1)],
                               sources=['src/heatshrink.pyx',
                                        'heatshrink/heatshrink_encoder.c',
                                        'heatshrink/heatshrink_decoder.c'])
