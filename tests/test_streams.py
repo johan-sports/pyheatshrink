cdc33c4aeb8bd53b73dab2c84ab0488f51dfc602
Antonis Kalou <kalouantonis@gmail.com>
2016-11-07 19:14:56 +0200

    Removed SOME duplication from the tests

--- tests/test_streams.py
+++ tests/test_streams.py
@@ -15,7 +15,13 @@ TEST_FILENAME = 'test_{}_tmp'.format(os.getpid())
 # TODO: Add test to see if compression parameters are
 # TODO: passed to Reader/Writer
 class EncodedFileTest(unittest.TestCase):
+    def setUp(self):
+        self.fp = EncodedFile(TEST_FILENAME, 'wb')
+
     def tearDown(self):
+        if self.fp:
+            self.fp.close()
+
         # Cleanup temporary file
         if os.path.exists(TEST_FILENAME):
             os.unlink(TEST_FILENAME)
@@ -32,16 +38,14 @@ class EncodedFileTest(unittest.TestCase):
                 EncodedFile(data, mode=mode)
 
     def test_round_trip(self):
-        write_str = 'Testing\nAnd Stuff'
+        write_str = b'Testing\nAnd Stuff'
 
         # TODO: Consider using EncodedFile with StringIO
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(write_str)
+        self.fp.write(write_str)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            read_str = fp.read()
-
-        self.assertEqual(write_str, read_str)
+        self.fp = EncodedFile(TEST_FILENAME)
+        self.assertEqual(self.fp.read(), write_str)
 
     def test_with_large_files(self):
         test_sizes = [1000, 10000, 100000]
@@ -75,203 +79,197 @@ class EncodedFileTest(unittest.TestCase):
             self.assertTrue(len(fp.read()) > 0)
 
     def test_closed_true_when_file_closed(self):
-        fp = EncodedFile(TEST_FILENAME, mode='wb')
-        self.assertTrue(not fp.closed)
-        fp.close()
-        self.assertTrue(fp.closed)
+        self.assertFalse(self.fp.closed)
+        self.fp.close()
+        self.assertTrue(self.fp.closed)
 
     def test_context_manager(self):
         with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write('Testing\n')
-            fp.write('One, two...')
+            fp.write(b'Testing\n')
+            fp.write(b'One, two...')
 
         self.assertTrue(fp.closed)
 
     def test_operations_on_closed_file(self):
-        fp = EncodedFile(TEST_FILENAME, mode='wb')
-        fp.close()
-        self.assertRaises(ValueError, fp.write, 'abcde')
-        self.assertRaises(ValueError, fp.seek, 0)
+        self.fp.close()
+        self.assertRaises(ValueError, self.fp.write, b'abcde')
+        self.assertRaises(ValueError, self.fp.seek, 0)
 
-        fp = EncodedFile(TEST_FILENAME, 'rb')
-        fp.close()
-        self.assertRaises(ValueError, fp.read)
-        self.assertRaises(ValueError, fp.seek, 0)
+        self.fp = EncodedFile(TEST_FILENAME, 'rb')
+        self.fp.close()
+        self.assertRaises(ValueError, self.fp.read)
+        self.assertRaises(ValueError, self.fp.seek, 0)
 
     def test_cannot_write_in_read_mode(self):
         # Write some junk data
-        with open(TEST_FILENAME, 'wb') as fp:
-            fp.write(b'abcde')
+        self.fp.write(b'abcde')
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            self.assertTrue(fp.readable())
-            self.assertTrue(not fp.writable())
-            self.assertRaises(IOError, fp.write, 'abcde')
+        self.fp = EncodedFile(TEST_FILENAME)
+        self.assertTrue(self.fp.readable())
+        self.assertFalse(self.fp.writable())
+        self.assertRaises(IOError, self.fp.write, b'abcde')
 
     def test_cannot_read_in_write_mode(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            self.assertTrue(fp.writable())
-            self.assertTrue(not fp.readable())
-            self.assertRaises(IOError, fp.read)
+        self.assertTrue(self.fp.writable())
+        self.assertFalse(self.fp.readable())
+        self.assertRaises(IOError, self.fp.read)
 
     def test_seeking_forwards(self):
         contents = random_string(250)
+        self.fp.write(contents)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(contents)
-
-        with EncodedFile(TEST_FILENAME) as fp:
-            self.assertEqual(fp.read(100), contents[:100])
-            fp.seek(50, io.SEEK_CUR)  # Move 50 forwards
-            self.assertEqual(fp.read(100), contents[-100:])
+        self.fp = EncodedFile(TEST_FILENAME)
+        self.assertEqual(self.fp.read(100), contents[:100])
+        self.fp.seek(50, io.SEEK_CUR)  # Move 50 forwards
+        self.assertEqual(self.fp.read(100), contents[-100:])
 
     def test_seeking_backwards(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write('abcde')
+        self.fp.write(b'abcde')
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            contents = fp.read(100)
-            # Reset and re-read
-            fp.seek(0)
-            self.assertEqual(fp.read(100), contents)
+        self.fp = EncodedFile(TEST_FILENAME)
+        contents = self.fp.read(100)
+        # Reset and re-read
+        self.fp.seek(0)
+        self.assertEqual(self.fp.read(100), contents)
 
     def test_seeking_from_end(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(LARGE_PARAGRAPH)
+        self.fp.write(LARGE_PARAGRAPH)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            self.assertEqual(fp.read(100), LARGE_PARAGRAPH[:100])
-            fp.seek(-100, io.SEEK_END)
-            self.assertEqual(fp.read(100), LARGE_PARAGRAPH[-100:])
+        self.fp = EncodedFile(TEST_FILENAME)
+        self.assertEqual(self.fp.read(100), LARGE_PARAGRAPH[:100])
+        self.fp.seek(-100, io.SEEK_END)
+        self.assertEqual(self.fp.read(100), LARGE_PARAGRAPH[-100:])
 
     def test_tell(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            bytes_written = fp.write('abcde')
-            self.assertEqual(fp.tell(), bytes_written)
+        bytes_written = self.fp.write(b'abcde')
+        self.assertEqual(self.fp.tell(), bytes_written)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            fp.read(3)
-            self.assertEqual(fp.tell(), 3)
+        self.fp = EncodedFile(TEST_FILENAME)
+        self.fp.read(3)
+        self.assertEqual(self.fp.tell(), 3)
 
     def test_peek(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(LARGE_PARAGRAPH)
+        self.fp.write(LARGE_PARAGRAPH)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            pdata = fp.peek()
-            self.assertNotEqual(len(pdata), 0)
-            self.assertTrue(LARGE_PARAGRAPH.startswith(pdata))
-            self.assertEqual(fp.read(), LARGE_PARAGRAPH)
+        self.fp = EncodedFile(TEST_FILENAME)
+        pdata = self.fp.peek()
+        self.assertNotEqual(len(pdata), 0)
+        self.assertTrue(LARGE_PARAGRAPH.startswith(pdata))
+        self.assertEqual(self.fp.read(), LARGE_PARAGRAPH)
 
     #################
     # Reading
     #################
     def test_read_whole_file(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(LARGE_PARAGRAPH)
+        self.fp.write(LARGE_PARAGRAPH)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            read_str = fp.read()
-
+        self.fp = EncodedFile(TEST_FILENAME)
+        read_str = self.fp.read()
         self.assertEqual(LARGE_PARAGRAPH, read_str)
 
     def test_read_buffered(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(LARGE_PARAGRAPH)
+        self.fp.write(LARGE_PARAGRAPH)
+        self.fp.close()
 
         READ_SIZE = 128
         offset = 0
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            read_buf = functools.partial(fp.read, READ_SIZE)
+        self.fp = EncodedFile(TEST_FILENAME)
+        read_buf = functools.partial(self.fp.read, READ_SIZE)
 
-            for i, contents in enumerate(iter(read_buf, '')):
-                offset = READ_SIZE * i
-                self.assertEqual(
-                    contents,
-                    LARGE_PARAGRAPH[offset: offset + READ_SIZE]
-                )
+        for i, contents in enumerate(iter(read_buf, '')):
+            offset = READ_SIZE * i
+            self.assertEqual(
+                contents,
+                LARGE_PARAGRAPH[offset: offset + READ_SIZE]
+            )
 
     def test_read_one_char(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(LARGE_PARAGRAPH)
+        self.fp.write(LARGE_PARAGRAPH)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            for c in LARGE_PARAGRAPH:
-                self.assertEqual(fp.read(1), c)
+        self.fp = EncodedFile(TEST_FILENAME)
+        for c in LARGE_PARAGRAPH:
+            self.assertEqual(self.fp.read(1), c)
 
     def test_read1(self):
         raise AssertionError('TODO')
 
     def test_readinto(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write('abcde')
+        self.fp.write(b'abcde')
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            a = array.array('b', b'x' * 10)  # Fill with junk
-            n = fp.readinto(a)
-            try:
-                # Python 3
-                self.assertEqual('abcde', a.tobytes()[:n])
-            except AttributeError:
-                self.assertEqual('abcde', a.tostring()[:n])
+        self.fp = EncodedFile(TEST_FILENAME)
+        a = array.array('b', b'x' * 10)  # Fill with junk
+        n = self.fp.readinto(a)
+        try:
+            # Python 3
+            self.assertEqual(b'abcde', a.tobytes()[:n])
+        except AttributeError:
+            self.assertEqual(b'abcde', a.tostring()[:n])
 
     def test_readline(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(LARGE_PARAGRAPH)
+        self.fp.write(LARGE_PARAGRAPH)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            lines = LARGE_PARAGRAPH.splitlines()
+        self.fp = EncodedFile(TEST_FILENAME)
+        lines = LARGE_PARAGRAPH.splitlines()
 
-            for index, line in enumerate(iter(fp.readline, '')):
-                self.assertEqual(line, lines[index] + '\n')
+        for index, line in enumerate(iter(self.fp.readline, '')):
+            self.assertEqual(line, lines[index] + '\n')
 
     def test_readline_iterator(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(LARGE_PARAGRAPH)
+        self.fp.write(LARGE_PARAGRAPH)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            lines = LARGE_PARAGRAPH.splitlines()
+        self.fp = EncodedFile(TEST_FILENAME)
+        lines = LARGE_PARAGRAPH.splitlines()
 
-            for i, line in enumerate(fp):
-                self.assertEqual(line, lines[i] + '\n')
+        for i, line in enumerate(self.fp):
+            self.assertEqual(line, lines[i] + '\n')
 
     def test_readlines(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            fp.write(LARGE_PARAGRAPH)
+        self.fp.write(LARGE_PARAGRAPH)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            lines = fp.readlines()
-            self.assertEqual(''.join(lines), LARGE_PARAGRAPH)
+        self.fp = EncodedFile(TEST_FILENAME)
+        lines = self.fp.readlines()
+        self.assertEqual(''.join(lines), LARGE_PARAGRAPH)
 
     #################
     # Writing
     #################
     def test_write(self):
-        with EncodedFile(TEST_FILENAME, mode='wb') as fp:
-            BUFFER_SIZE = 16
-            # StringIO makes it easy to buffer
-            text_buf = io.BytesIO(LARGE_PARAGRAPH.encode('utf8'))
+        BUFFER_SIZE = 16
+        # StringIO makes it easy to buffer
+        text_buf = io.BytesIO(LARGE_PARAGRAPH.encode('utf8'))
 
-            while True:
-                chunk = text_buf.read(BUFFER_SIZE)
+        while True:
+            chunk = text_buf.read(BUFFER_SIZE)
 
-                if not chunk:
-                    break
+            if not chunk:
+                break
 
-                fp.write(chunk)
+            self.fp.write(chunk)
+        self.fp.close()
 
-        with EncodedFile(TEST_FILENAME) as fp:
-            self.assertEqual(fp.read(), LARGE_PARAGRAPH)
+        self.fp = EncodedFile(TEST_FILENAME)
+        self.assertEqual(self.fp.read(), LARGE_PARAGRAPH)
 
     def test_remaining_data_flushed_on_close(self):
-        fp = EncodedFile(TEST_FILENAME, mode='wb')
-        fp.write(LARGE_PARAGRAPH)
+        self.fp.write(LARGE_PARAGRAPH)
 
         with open(TEST_FILENAME) as read_fp:
             self.assertEqual(len(read_fp.read()), 0)
 
-        fp.close()
+        self.fp.close()
 
         with open(TEST_FILENAME) as read_fp:
             self.assertTrue(len(read_fp.read()) > 0)
