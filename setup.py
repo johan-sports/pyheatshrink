16a9eeac187788ef820eaef2214d950e9d45ea56
Jacob Siverskog <jacob@teenage.engineering>
2020-11-03 16:17:38 +0100

    Bump cython to support newer Python versions
    This adds support for Python 3.8 and 3.9.

--- setup.py
+++ setup.py
@@ -67,6 +67,8 @@ setup(name='Heatshrink',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
       ],
 
       keywords='compression binding heatshrink LZSS',
